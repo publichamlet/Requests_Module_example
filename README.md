40d602823e54bcae1b67c7095a07a624e1210a33
public_hamlet <anooj4trade@gmail.com>
2022-09-14 06:49:33 +0530

    web scraping by request

---
+++ README.md
@@ -0,0 +1,9 @@
+# Web Scraping using python Requests library
+
+> Simple web scraping using request library
+> See the following files extracted from https://quotes.toscrape.com/
+>> requests_module/quotes.csv <br>
+>> requests_module/quote_20_pages.txt <br>
+>> requests_module/quote_1st_page.txt <br>
+>> requests_module/author_20_pages.txt <br>
+>> requests_module/author_1st_page.txt <br>
---
+++ test.py
@@ -0,0 +1,75 @@
+import sys, os
+import requests
+import csv
+
+if sys.platform.startswith('linux'):
+    os.chdir(os.path.dirname(__file__))
+
+
+# url = 'https://quotes.toscrape.com/'
+
+# req = requests.get(url)
+
+# print(req.status_code)
+
+# html = req.text
+
+# with open('quote_1st_page.txt', 'w') as txt:
+#     for line in html.split('\n'):
+#         if '<span class="text" itemprop="text">' in line:
+#             line = line.replace('<span class="text" itemprop="text">', '')
+#             line = line.replace('</span>', '').strip()
+#             txt.write(line)
+#             txt.write('\n')
+
+# with open('author_1st_page.txt', 'w') as txt:
+#     for line in html.split('\n'):
+#         if '<small class="author" itemprop="author">' in line:
+#             line = line.replace('<span>by <small class="author" itemprop="author">', '')
+#             line = line.replace('</small>', '').strip()
+#             txt.write(line)
+#             txt.write('\n')
+
+quotes = list()
+authors = list()
+
+for i in range(20):
+    url = f'https://quotes.toscrape.com/page/{i}/'
+
+    req = requests.get(url)
+
+    print(f'Status:{req.status_code} Extracting HTML page {i+1}...........')
+
+    html = req.text
+
+
+    with open('quote_20_pages.txt', 'a') as txt:
+        for line in html.split('\n'):
+            if '<span class="text" itemprop="text">' in line:
+                line = line.replace('<span class="text" itemprop="text">', '')
+                quote = line.replace('</span>', '').strip()
+                quotes.append(quote)
+                # txt.write(line)
+                # txt.write('\n')
+
+    with open('author_20_pages.txt', 'a') as txt:
+        for line in html.split('\n'):
+            if '<small class="author" itemprop="author">' in line:
+                line = line.replace('<span>by <small class="author" itemprop="author">', '')
+                author = line.replace('</small>', '').strip()
+                authors.append(author)
+                # txt.write(line)
+                # txt.write('\n')
+    
+with open('quotes.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Author', 'Quote'])
+    # for quote, author in list(zip(quotes, authors)):
+    #     field = {'Author': author, 'Quote': quote}
+    #     writer = csv.DictWriter(csvfile, fieldnames=['Author', 'Quote'])
+    #     writer.writerow(field)
+    for quote, author in list(zip(quotes, authors)):
+        field = [author, quote]
+        writer = csv.writer(csvfile)
+        writer.writerow(field)
+    
